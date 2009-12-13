a847d90a16eaacdeda7ba94e23c270f7a64c8093
Robert Lehmann <me@robertlehmann.de>
2009-12-13 22:21:15 +0100

    Added rudimentary IRC implementation.

--- contrib/irc.py
+++ contrib/irc.py
@@ -5,6 +5,8 @@ hi(
     desc = "Internet Relay Chat transport layer",
 )
 
+DEFAULT_PORT = 6667
+
 import socket
 
 @hook("send")
@@ -17,14 +19,36 @@ def send_message(message, dest):
             #+ spam queue für channels
             socket.send("PRIVMSG %s :%s" % (d, message))
 
-@thread
 @hook("startup")
+@thread
 def establish_connection():
     s = socket.socket()
-    if x.match("PRIVMSG * :!*"): #+ prefix aus der config laden
-        origin, command = x.matches()
+    send = lambda line: s.send(line + "\n")
+    server = remember("irc server")
+    port = remember("irc port", DEFAULT_PORT)
+    s.connect((server, port))
+    send("USER pyhkal pyhkal pyhkal :PyHKAL")
+    send("NICK PyHKAL2")
+    buf = ""
+    while 1:
+        buf += s.recv(1024)
+        while 1:
+            eolpos = buf.find("\r\n")
+            if eolpos == -1:
+                break
+            line = buf[:eolpos]
+            if line.startswith("PING :"):
+                send(line.replace("PING :", "PONG "))
+            elif line.startswith(":"):
+                line = line.split()
+                if line[1] == "376":
+                    for channel in remember("irc channels", []):
+                        send("JOIN %s" % channel)
+            buf = buf[eolpos+2:]
+#    if x.match("PRIVMSG * :!*"): #+ prefix aus der config laden
+#        origin, command = x.matches()
         #+ in WebMod: x.split("/")
-        dispatch_command(IRCChannel(origin), command)
-    else:
+#        dispatch_command(IRCChannel(origin), command)
+#    else:
 #        _, event, *args = x.split()
-        dispatch_event(event, args)
+#        dispatch_event(event, args)
