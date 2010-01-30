e7297b91cc799f2f2ffca1752b369f5d768f4670
chosenone <chosenone@luna.(none)>
2010-01-30 22:00:14 +0100

    some more custom callbacks, numeric events and so on

--- contrib/irc.py
+++ contrib/irc.py
@@ -8,6 +8,7 @@ hi(
 from twisted.application import internet
 from twisted.internet import protocol, reactor
 from twisted.words.protocols import irc
+from itertools import cycle
 
 DEFAULT_PORT = 6667
 DEFAULT_NICK = "pyhkal"
@@ -62,6 +63,8 @@ class IRCClient(irc.IRCClient):
                 dispatch_event("irc."+name, *args)
             if not hasattr(IRCClient,name):
                 setattr(self, name, obj)
+        self.whocounter = iter(cycle(xrange(1,1000)))
+        self.whocallbacks = {}
 
     def connectionMade(self):
         def send(msg):
@@ -90,6 +93,7 @@ class IRCClient(irc.IRCClient):
         for channel in self.channels:
             self.join(channel)
     def modeChanged(self, user, channel, set, modes, args):
+        print "<>", modes, repr(args)
         dispatch_event("irc.modechange", user, channel, set, modes, args)
         if set:
             dispatch_event("irc.setmode", user, channel, modes, args)
@@ -101,29 +105,42 @@ class IRCClient(irc.IRCClient):
         colontuple = data.split(':')
         numeric = spacetuple[1]
         #params = colontuple[2]
-        print "<", numeric, ">", data       
+        #print "<", numeric, ">", data       
         if numeric == '353':
-            dispatch_event("irc.names", colontuple[2].split(' ')) # dirty list! like 
+            dispatch_event("irc.names", spacetuple[4], colontuple[2].split(' ')) # dirty list! like ['@ChosenOne','+npx', 'crosbow']
+        if numeric == "366":
+            dispatch_event('irc.endofnames', spacetuple[3] )
+    
+        if numeric == "354":
+            dispatch_event('irc.whorep', spacetuple[3:]) # generic hook, that will give you the answer, without knowing the question :D
+            
 
+    def getInfo(self, target, callback_result, callback_end):
+        ID = whocounter.next()
+        whocallbacks[ID] = (ID, callback_result, callback_end)
+        if target[0] == '#':
+            self.send("WHO %s %nafuhr,%s" % (target, ID))
+        else: # nickname
+            self.send("WHO %s, %nafuhr,%s" % (target, ID))
+        
+"""
+
+id ident host nickname flags auth/0 realname
+
+<< WHO #pyhkal, %nafuhrt,666
+>> :clanserver4u1.de.quakenet.org 354 ChosenOne 666 ~ai p4FE4D27B.dip.t-dialin.net ai H 0 :Always Innovating
+>> :clanserver4u1.de.quakenet.org 354 ChosenOne 666 ~olpc p4FE4D27B.dip.t-dialin.net olpc H 0 :Unknown
+>> :clanserver4u1.de.quakenet.org 354 ChosenOne 666 ~chosenone client-vpn-56.rz.ruhr-uni-bochum.de ChosenOne H+ 0 :chosenone
+>> :clanserver4u1.de.quakenet.org 354 ChosenOne 666 PyHKAL2 server3.raumopol.de PyHKAL3 H@ 0 :PyHKAL
+>> :clanserver4u1.de.quakenet.org 354 ChosenOne 666 npx noopman.users.quakenet.org NPX G+x noopman :NPENIX
+>> :clanserver4u1.de.quakenet.org 354 ChosenOne 666 ~saldana smitty.knid.net saldana H@ 0 :saldana
+>> :clanserver4u1.de.quakenet.org 354 ChosenOne 666 StruC server3.raumopol.de StruC H@ StruC :StruC
+>> :clanserver4u1.de.quakenet.org 354 ChosenOne 666 stargaming server3.raumopol.de starGaming H@ stargaming :stargaming
+>> :clanserver4u1.de.quakenet.org 315 ChosenOne #pyhkal, :End of /WHO list.
 
 """
-2010-01-30 18:54:35+0100 [IRCClient,client] < JOIN > :PyHKAL3!PyHKAL2@server3.raumopol.de JOIN :#p
-
-2010-01-30 18:54:35+0100 [IRCClient,client] < 353 > :port80a.se.quakenet.org 353 PyHKAL3 = #p :@Antesz @BMCT|JP`off CaTeYe PyHKAL3 @Q SaNci Scolo WA|4130 @esad gNu|boe @mark` @mrk` @si_- soma @wowbot @|Qlogged|
-2010-01-30 18:54:35+0100 [IRCClient,client] < 366 > :port80a.se.quakenet.org 366 PyHKAL3 #p :End of /NAMES list.
 
 
-
-354 <ich> <ziel> <auth>
-315 <end of who>
-
-
-353 channel names
-366 "end of names list"
-
-":port80a.se.quakenet.org 474 PyHKAL3 #' :Cannot join channel, you are banned (+b)"
-
-"""
 
 class IRCClientFactory(protocol.ReconnectingClientFactory):
     protocol = IRCClient
