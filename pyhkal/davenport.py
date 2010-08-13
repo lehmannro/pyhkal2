98e132322231cce9d0fc323dc4b6d90651df7301
Robert Lehmann <me@robertlehmann.de>
2010-08-13 21:59:32 +0200

    IM IN YR COUCH ADDIN VIEWS

--- pyhkal/davenport.py
+++ pyhkal/davenport.py
@@ -5,6 +5,7 @@ Document storage.
 
 """
 
+from twisted.web.client import HTTPClientFactory
 from twisted.python.failure import Failure
 import paisley
 from base64 import b64encode
@@ -12,12 +13,17 @@ from base64 import b64encode
 class Davenport(paisley.CouchDB):
     def __init__(self, host, db, user, passwd, port=5984):
         paisley.CouchDB.__init__(self, host, port, dbName=db)
-        self.auth = "Basic %s:%s" % (user, passwd)
+        self.auth = "Basic " + b64encode("%s:%s" % (user, passwd))
 
     def _getPage(self, uri, **kwargs):
-        headers = kwargs.setdefault('headers', {})
-        headers['Authorization'] = self.auth
-        return paisley.CouchDB._getPage(self, uri, **kwargs)
+        # paisley steals our headers so this is copypasta
+        url = self.url_template % (uri,)
+        kwargs["headers"] = {"Accept": "application/json",
+            "Authorization": self.auth}
+        factory = HTTPClientFactory(url, **kwargs)
+        from twisted.internet import reactor
+        reactor.connectTCP(self.host, self.port, factory)
+        return factory.deferred
 
     def order(self, module, by, map_fun, reduce_fun=None):
         #XXX reduce_fun
@@ -27,6 +33,6 @@ class Davenport(paisley.CouchDB):
         def add_view(doc):
             if isinstance(doc, Failure):
                 doc = {}
-            self.addViews(doc, {by: payload})
+            self.addViews(doc, {by: dict(map=payload)})
             self.saveDoc(doc, docId=docname)
         self.openDoc(docname).addBoth(add_view)
