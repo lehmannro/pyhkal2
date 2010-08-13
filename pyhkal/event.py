2d7ca8f1060db0f7f67149d39d62367ee40ed7f3
Jan-Oliver Kaiser <j@n.no>
2010-08-13 16:09:19 +0200

    fixed syntax errors in event.py and IRCUser.py

--- pyhkal/event.py
+++ pyhkal/event.py
@@ -1,7 +1,7 @@
 #1/usr/bin/env python
 
 class Event(object):
-    def __init__(self, target. source, content)
+    def __init__(self, target, source, content):
         self.target = target
         self.source = source
         self.content = content
