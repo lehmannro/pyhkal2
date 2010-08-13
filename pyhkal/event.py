5da459c0c677cbf8315b3c71325e838d73e64f8a
Jan-Oliver Kaiser <j@n.no>
2010-08-13 15:11:23 +0200

    reworked event.py

--- pyhkal/event.py
+++ pyhkal/event.py
@@ -1,7 +1,10 @@
+#1/usr/bin/env python
+
 class Event(object):
-    target
-    source
-    content
+    def __init__(self, target. source, content)
+        self.target = target
+        self.source = source
+        self.content = content
 
     def reply(self):
         pass
