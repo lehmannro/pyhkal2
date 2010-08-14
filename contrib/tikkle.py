fb0d23979dd703a30fe69eb131de5f4a5c1f19fc
npx <npx@g0x21k.(none)>
2010-08-14 14:28:25 +0200

    Tikkle yiyiyiyi

--- contrib/tikkle.py
+++ contrib/tikkle.py
@@ -11,7 +11,7 @@
     user.py uebernimmt identitiy handling!!11!!1EINSELF
 """
 from twisted.internet import defer
-
+import re
 __version__ = "0.1a"
 __requires__ = ["irc"]
 
@@ -27,38 +27,42 @@ chaos("PenisViewUSER",
 @hook('privmsg')
 @defer.inlineCallbacks
 def startTheTikkleFun(event):
-    if (isinstance(event,irc.IRCChannel)):
-
+    event.reply("InFun")
+    if (isinstance(event.target,irc.IRCChannel)):
+        event.reply("InIf")
         # 1) Phrase basteln!
         msg = event.content
 
         # 2) Ist der User schon eingeloggt? bzw identity gelinkt?
-        ident = event.source.identity
+        ident = event.source.identity if (hasattr(event.source,"identity")) else None
 
         ######## User ist eingeloggt
         if (ident != None):
-            d = davenport.openDoc(event.ident.docid)
+            event.reply("InIf2")
+            d = davenport.openDoc(event.source.identity.docid)
             doc = yield d
 
-            loginRE = re.compile(doc.tikkle.login)
-            logoutRE = re.compile(doc.tikkle.logout)
-            afkRE = re.compile(doc.tikkle.afk)
-
+            loginRE = re.compile(doc["tikkle"]["login"])
+            logoutRE = re.compile(doc["tikkle"]["logout"])
+            afkRE = re.compile(doc["tikkle"]["afk"])
+            event.reply("PreMatch")
             if (loginRE.match(event.content) != None):
+                event.reply("doStuff() - ehehehe, Freddy B")
                 doStuff()
             elif (logoutRE.match(event.content) != None):
-                event.source.ident.avatars.remove(event.source)
-                event.source.ident = None
-                print "Logged out?!"
+                event.source.identity.avatars.remove(event.source)
+                event.source.identity = None
+                event.reply("Logged out?!")
             elif (afkRE.match(event.content) != None):
                 # set last activity
-                print "Set Last activity!"
+                event.reply("Set Last activity!")
 
-@hook('privmsg')
-def mumupupu(event):
-    print "FUBARUBARUBARUBARUBRAURBA PENIS!-------------"
-    event.reply('fubardubar')
+def doStuff():
+    print "DOOOSTUUUUUUFFFFFFF!!!111222"
 
-@hook("user.loggedin")
-def doStuff(acc):
-    irc.notice(origin.user, "hi %s! DIGESTS UND SO!" % acc)
+@hook('privmsg',expr='npx')
+def MuupDuup(event):
+    if (event.source.nick == 'npx'):
+        event.source.identity = Identity("107097e10a2cacb7caa6d9d04d7ed8c7")
+        event.source.identity.link(event.source)
+        print "Locked n' Linked!"
