d9e557bdb123786d8e5199061801854dc46f60e6
Frederik B <Frederik.Braun@ruhr-uni-bochum.de>
2010-08-15 14:20:02 +0200

    admin now automatically linked

--- contrib/admin.py
+++ contrib/admin.py
@@ -1,50 +1,67 @@
 # coding: utf-8
 
 from pyhkal import shopping
+from twisted.internet import defer
+
+#TODO: Change hooks to commands
 
 __version__ = "0.1"
 __requires__ = ["irc"]
 
+@defer.inlineCallbacks
 def isadmin(source):
-    # return True
-    return source.nick == "saldana"
-    # return hasattr(source, identity) and hasattr(source.identity, admin) and source.identity.admin
+    if not source.identity:
+        defer.returnValue(False) # cant be admin, haz no identity
+    else: # check if identity is an admin 
+        d = source.identity.fetch()
+        identity = yield d
+        defer.returnValue(identity.get(u'admin',False))
 
 
-@hook("privmsg", expr="!load\s.+")
+@hook("message", expr="!load\s.+")
+@defer.inlineCallbacks
 def load_module(event):
-    if isadmin(event.source):
+    admin = yield isadmin(event.source)
+    if admin:
         for module in event.content.split(" ")[1:]:
             try:
                 shopping.buy(module)
             except BaseException as err: # gotta catch 'm all.
                 event.reply("Error: %s" % err)
 
-@hook("privmsg", expr="!reload\s.+")
+@hook("message", expr="!reload\s.+")
+@defer.inlineCallbacks
 def reload_module(event):
-    if isadmin(event.source):
+    admin = yield isadmin(event.source)
+    if admin:
         for module in event.content.split(" ")[1:]:
             shopping.revoke(module)
 
-@hook("privmsg", expr="!unload\s.+")
+@hook("message", expr="!unload\s.+")
+@defer.inlineCallbacks
 def unload_module(event):
-    if isadmin(event.source):
+    admin = yield isadmin(event.source)
+    if admin:
         for module in event.content.split(" ")[1:]:
             shopping.renew(module)
 
-@hook("privmsg", expr="!eval\s.+")
+@hook("message", expr="!eval\s.+") 
+@defer.inlineCallbacks             
 def eval_code(event):
-    if isadmin(event.source):
+    admin = yield isadmin(event.source)
+    if admin:
         try:
             event.reply(eval(event.content.split(" ", 1)[1]))
         except Exception as err: # gotta catch 'm all.
             event.reply("Error: %s" % err)
 
-@hook("privmsg", expr="!exec\s.+")
+@hook("message", expr="!exec\s.+")
+@defer.inlineCallbacks
 def exec_code(event):
-    if isadmin(event.source):
+    admin = yield isadmin(event.source)
+    if admin:
         exec event.content.split(" ", 1)[1] in globals()
 
-@hook("privmsg", expr="!addidentiy")
+@hook("message", expr="!addidentiy") # "Was hat sich der Autor dabei gedacht?"
 def foo(event):
     pass
