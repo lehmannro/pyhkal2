404b97627e34f1f810ea05bde207e3f6e78e61ba
Your Name <mail@janno-kaiser.de>
2010-08-25 01:19:54 +0200

    speedcubing: dirty hack to stop *cube from stopping the timer immediately

--- contrib/speedcubing.py
+++ contrib/speedcubing.py
@@ -11,11 +11,13 @@ __version__ = "0.2"
 @hook("message", expr=r'^\*cube\b')
 def startCubeTimer(event, r):
     if not hasattr(event.source, 'cube'):
-        event.source.cube = time.time()
+        event.source.cube = (time.time(), event)
 
 @hook("message")
 def stopCubeTimer(event):
     if hasattr(event.source, 'cube'):
-        timed = time.time() - event.source.cube
-        event.reply("%s: %.2g" % (event.source.nick, timed))
-        del event.source.cube
+        starting_time, trigger_event = event.source.cube
+        if event != trigger_event:
+            timed = time.time() - starting_time 
+            event.reply("%s: %.2g" % (event.source.nick, timed))
+            del event.source.cube
