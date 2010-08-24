b7146d892d4143b009b74839623ba52773c32307
Your Name <mail@janno-kaiser.de>
2010-08-25 01:08:12 +0200

    I\'m in your speedcubing mod, fixing your regex

--- contrib/speedcubing.py
+++ contrib/speedcubing.py
@@ -8,7 +8,7 @@ import time #XXX consult event.time instead of time.time
 
 __version__ = "0.2"
 
-@hook("message", expr=r'^*cube\b')
+@hook("message", expr=r'^\*cube\b')
 def startCubeTimer(event, r):
     if not hasattr(event.source, 'cube'):
         event.source.cube = time.time()
