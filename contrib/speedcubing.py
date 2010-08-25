230ffa538f5e267ec09863026673c8aeab8ad488
Manuel Fuhr <zod@knid.net>
2010-08-25 18:39:53 +0200

    ported scramble algorithm

--- contrib/speedcubing.py
+++ contrib/speedcubing.py
@@ -6,6 +6,35 @@
 
 __version__ = "0.3"
 
+from random import choice
+
+def cubescramble(num):
+    MOVES = [["U", "D"], ["R", "L"], ["F", "B"]]
+    MOVEMODS = ["", "", "2", "'"] # no prefix is twice as likely
+    scramble = []
+    for x in xrange(num):
+        d = {}
+
+        axes = range(3)
+        # axis came two times in a row, enough is enough
+        if len(scramble) > 1 and scramble[-2]['axis'] == scramble[-1]['axis']:
+            axes.remove(scramble[-1]['axis'])
+        d['axis'] = choice(axes)
+
+        moves = range(2)
+        # same axis as last time, don't make the same move
+        if len(scramble) > 0 and d['axis'] == scramble[-1]['axis']:
+            moves.remove(scramble[-1]['move'])
+        d['move'] = choice(moves)
+
+        d['movemod'] = choice(range(len(MOVEMODS)))
+        scramble.append(d)
+    return " ".join(MOVES[d['axis']][d['move']] + MOVEMODS[d['movemod']] for d in scramble)
+
+@register("3")
+def getCubeScramble(event):
+    event.reply("Try %s" % cubescramble(25))
+
 @hook("message", expr=r'^\*cube\b')
 def startCubeTimer(event, r):
     if not hasattr(event.source, 'cube'):
