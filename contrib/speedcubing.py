873d892a4f0a614481d20024e5b68a8a30faeb4d
Frederik B <Frederik.Braun@ruhr-uni-bochum.de>
2010-08-25 16:31:54 +0200

    recent update breaks speedcubing mod - it is event.content, not event.message :), also we moved to timedelta-objects for a beauty output

--- contrib/speedcubing.py
+++ contrib/speedcubing.py
@@ -4,19 +4,19 @@
 
 """
 
-import time #XXX consult event.time instead of time.time
+import datetime #XXX consult event.time instead of datetime.time
 
 __version__ = "0.2"
 
 @hook("message", expr=r'^\*cube\b')
 def startCubeTimer(event, r):
     if not hasattr(event.source, 'cube'):
-        event.source.cube = time.time()
+        event.source.cube = datetime.datetime.now()
 
 @hook("message")
 def stopCubeTimer(event):
     if hasattr(event.source, 'cube'):
-        if not event.message.startswith('*cube'):
-            timed = time.time() - event.source.cube
-            event.reply("%s: %.2g" % (event.source.nick, timed))
+        if not event.content.startswith('*cube'):
+            timed = datetime.datetime.now() - event.source.cube
+            event.reply("%s: %s" % (event.source.nick, timed))
             del event.source.cube
