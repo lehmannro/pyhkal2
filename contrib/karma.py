d12521a0991e0aef34a32eb5e94fa209a82100de
Robert Lehmann <me@robertlehmann.de>
2010-09-09 15:12:01 +0200

    Cleaned up karma module.

--- contrib/karma.py
+++ contrib/karma.py
@@ -25,54 +25,61 @@ getKarma = chaos("getKarma","""
 @defer.inlineCallbacks
 def karma_edit(event, wort, delta):
     wort = wort.lower()
+
+    # fetch old value, if any
     try:
         result = yield getKarma(key=wort, include_docs='true')
     except Error:
         result = dict(rows=0)
-    #print "DB antwort fuer getkarma key=", wort, 'ist' , result
-    if result[u'rows']: # karma erhöhen
-        entry = result[u'rows'][0][ u'doc'] #[u'id']
-        #print "es gibt schon karma fuer", wort, entry
+
+    # existing entry found;  update
+    if result[u'rows']:
+        # looking for the key ensures there is exactly *one* result row
+        entry = result[u'rows'][0]['doc']
+
         karmaspam = remember("karma timeout", TIMEOUT)
-        if (time.time() - entry[u'updated_at']) > karmaspam:
+
+        # Karma modifications MUST only occur at set intervals.  These are
+        # purely game dynamics and do *not* facilitate any spam protection.
+        if time.time() - entry[u'updated_at'] > karmaspam:
             entry[u'value'] += delta
-            entry[u'updated_at'] = time.time()      # printable wirds durch time.ctime(updated_at)
-            #print "nun speicher ich das neue doch", entry
-            yield davenport.saveDoc(entry) # entry in DB stecken
+            entry[u'updated_at'] = time.time()
+            # Note: this is an update because the document carries its docid
+            yield davenport.saveDoc(entry)
+            # XXX I'm unsure if waiting for the database is really required
+            #     here.  We should just continue.
             event.reply("%s hat nun einen karmawert von %s" % (wort, entry[u'value']))
         else:
             rest = seconds2string(karmaspam - (time.time() - entry[u'updated_at']))
             event.reply("%s ist noch %s blockiert" % (wort, rest))
-    else: # neuen adden
-        #print "wir adden karma neu"
-        entry = {u'doctype': 'karma', u'name': wort, u'value': delta, u'updated_at': time.time(), u'created_at': time.time() }
-        yield davenport.saveDoc(entry) # entry in db stecken
+    # item not found;  create new
+    else:
+        entry = dict(doctype='karma', name=wort, value=delta
+                     updated_at=time.time(), created_at=time.time())
+        yield davenport.saveDoc(entry) # XXX same qualifier as above
         event.reply("%s hat nun einen karmawert von %s" % (wort, delta))
 
-@hook("message", r"(\S\S+)\+\+(?:\s|$)") #r"\b(\w+)\+\+"
+@hook("message", r"(\S\S+)\+\+(?:\s|$)")
 def karma_add(event, wort):
     return karma_edit(event, wort, +1)
 
-@hook("message", r"([\S]\S+)--(?:\s|$)") # r"\b(\w+)--"
+@hook("message", r"([\S]\S+)--(?:\s|$)")
 def karma_del(event, wort):
     return karma_edit(event, wort, -1)
 
-@hook("message", r"(\S\S+)==(?:\s|$)") # r"\b(\w+)=="
+@hook("message", r"(\S\S+)==(?:\s|$)")
 @defer.inlineCallbacks
 def karma_say(event, wort):
     wort = wort.lower()
     try:
         result = yield getKarma(key=wort)
     except Error:
-        result = dict(rows=0)
-    if result[u'rows']: # karma erhöhen
-        value = result[u'rows'][0][u'value']
+        result = dict(rows=0) # fall through the following if statement
+    if result[u'rows']:
+        value = result['rows'][0]['value']
     else:
         value = 0
     event.reply("%s hat einen karmawert von %s" % (wort, value))
 
 def seconds2string(sec):
     return str(datetime.timedelta(seconds=sec))[2:]
-#(\S\S+\+\+)(?:\s|$)
-#([\S]\S+--)(?:\s|$)
-#(\S\S+==)(?:\s|$)
