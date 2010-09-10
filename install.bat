42588899b90aab1b78d65e2c369218fb6c22c66b
unknown <crosbow@.(none)>
2010-09-10 19:39:24 +0200

    install.bat does not need clean and rebuild after changing python source

--- install.bat
+++ install.bat
@@ -17,5 +17,10 @@ echo [build] > var\pydistutils.cfg && echo compiler = mingw32 >> var\pydistutils
 var\Scripts\python setup.py --quiet install
 :run
 echo Running PyHKAL..
+REM Redistribute .py files
+rmdir /S /Q var\Lib\site-packages\pyhkal-0.0.0-py2.6.egg\pyhkal
+xcopy pyhkal var\Lib\site-packages\pyhkal-0.0.0-py2.6.egg\pyhkal /Q /Y /I
+xcopy contrib var\Lib\site-packages\pyhkal-0.0.0-py2.6.egg\pyhkal\contrib /Q /Y /I
+xcopy test var\Lib\site-packages\pyhkal-0.0.0-py2.6.egg\pyhkal\test /Q /Y /I
 var\Scripts\python var\Scripts\twistd.py -n pyhkal config.yaml
 :end
